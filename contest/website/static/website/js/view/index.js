f14da1a83f13b4905d7ebc8f829ae00fd86bc328
developer <developer@entpy.com>
2014-07-29 21:17:05 +0200

    added entpy universe background svg

--- contest/website/static/website/js/view/index.js
+++ contest/website/static/website/js/view/index.js
@@ -1,16 +1,20 @@
 $(document).ready(function(){
 	var s = Snap("#svg_container");
-	Snap.load("static/website/img/entpy-universe.svg", onSVGLoaded);
+	Snap.load("static/website/img/entpy-universe2.svg", onSVGLoaded);
        
 	function onSVGLoaded (svg){
 
 		s.append(svg);
+		var entpy_universe = s.select("#svg5571");
+		var entpy_logo = s.select("#g4538-6");
+
+		entpy_logo.animate({ transform: '' }, 2000);		
 
 		// adding background to div
 		// s.add(f);
 
 		// define svg main elements
-		var ufo = s.select("#g4213-4");
+		// var ufo = s.select("#g4213-4");
 		/*var logo = f.select("#g4538-6");
 		var galaxy = s.select("#g5065-5");
 		var computer = s.select("#g4219-3");
@@ -23,9 +27,9 @@ $(document).ready(function(){
 
 		ufo.attr({fill: "#5D9170"});*/
 		// main elements events
-                ufo.click(function () {
+                /*ufo.click(function () {
 			ufo.animate({y: -50,x: 10}, 1000);
-                });
+                });*/
 	}
 
 	/*var bigCircle = s.circle(150, 150, 100);
