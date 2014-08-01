cd92856380393e084bcd73166980f565b6487d25
developer <developer@entpy.com>
2014-08-01 02:04:14 +0200

    working on svg background

--- contest/website/static/website/js/view/index.js
+++ contest/website/static/website/js/view/index.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	// setting svg aspect ratio
 	// $("#svg > svg").attr("preserveAspectRatio", "xMinYMin slice");
 
-	var s = Snap("#svg");
+	var s = Snap("#svg_container");
 
 	// Lets create big circle in the middle:
 	/*var bigCircle = s.circle(100, 100, 100);
@@ -27,7 +27,8 @@ $(document).ready(function(){
 		// var viewBox_h = svgData["y2"] - svgData["y"];
 
 		s.select("#svg2").attr({
-			preserveAspectRatio : "xMinYMin slice"
+			preserveAspectRatio : "xMinYMin slice",
+			viewBox : "0 0 150 454.85983"
 		});
 
 		//$("#svg2").attr("height", viewBox_h * 10);
@@ -45,6 +46,8 @@ $(document).ready(function(){
 		// galaxy.animate( { transform: "matrix(0.1224681,0,0,0.1224681,265.99041,985.0055)" }, 1000 );
 		enableGalaxyAnimation(galaxy, 'r20', '-1916.3996,-641.4472', 5000);
 		enableGalaxyAnimation(galaxy2, 'r20', '-1966.5809,-604.8831', 2000);
+
+		s.select("#svg2").animate({ transform: "matrix(1,0,0,1,0,-114)" }, 5000);
 
 		var galaxy_details = s.select("#g33").getBBox();
 		// console.log(galaxy_details);
