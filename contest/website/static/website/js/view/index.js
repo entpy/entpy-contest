7f9a48c3696cfc58423a63bc8fd7ba951bb29356
developer <developer@entpy.com>
2014-08-04 18:27:52 +0200

    working on animation

--- contest/website/static/website/js/view/index.js
+++ contest/website/static/website/js/view/index.js
@@ -1,83 +1,79 @@
+// adding Modernizr svgasimg method to check if an svg could be used inside img tag
+Modernizr.addTest('svgasimg', document.implementation.hasFeature('http://www.w3.org/TR/SVG11/feature#Image', '1.1'));
+
 $(document).ready(function(){
 
-	// http://www.jsdb.io/animation/?sort=rating
-	initSnapSvg();
+	// validating svg image
+	validate_document_img();
 
-	// setting svg aspect ratio
-	// $("#svg > svg").attr("preserveAspectRatio", "xMinYMin slice");
-
-	// Lets create big circle in the middle:
-	/*var bigCircle = s.circle(100, 100, 100);
-
-	bigCircle.attr({
-		fill: "#bada55",
-		stroke: "#000",
-		strokeWidth: 5
-	});
-
-	var smallCircle = s.circle(180, 100, 100);
-
-	smallCircle.animate({r: 50}, 1000);*/
-	//Snap.load("static/website/img/entpy-universe2.svg", onSVGLoaded);
-
-	//function onSVGLoaded (svg){
-
-		// s.add(svg);
-
-		// var svgData = s.select("#g13").getBBox();
-		// var viewBox_h = svgData["y2"] - svgData["y"];
-
-		/*s.select("#svg2").attr({
-			preserveAspectRatio : "xMinYMin slice",
-			viewBox : "0 0 150 454.85983"
-		});*/
-
-		//$("#svg2").attr("height", viewBox_h * 10);
-
-		/*var entpy_universe = snapifiedSvg.select("#svg5571");
-		var entpy_logo = snapifiedSvg.select("#g4538-6");
-		var galaxy = snapifiedSvg.select("#path35");
-		var galaxy2 = snapifiedSvg.select("#path37");*/
-
-		// entpy logo entering XXX: OK
-		/*entpy_logo.transform('matrix(0.00,0,0,0.00,60,868.50045)');
-		entpy_logo.animate({ transform: 'matrix(0.10,0,0,0.10,35,868.50045)' }, 5000, mina.bounce);
-		*/
-
-		// galaxy.animate( { transform: "matrix(0.1224681,0,0,0.1224681,265.99041,985.0055)" }, 1000 );
-		/*enableGalaxyAnimation(galaxy, 'r20', '-1916.3996,-641.4472', 5000);
-		enableGalaxyAnimation(galaxy2, 'r20', '-1966.5809,-604.8831', 2000);
-
-		var galaxy_details = snapifiedSvg.select("#g33").getBBox();*/
-		// console.log(galaxy_details);
-
-	//}
-
-	function enableGalaxyAnimation(galaxy, radius, position, time) {
-		galaxy.animate({ transform: radius + "," + position }, time);
-	}
-
-	function resetGalaxyAnimation(galaxy, radius, position, time) {
-		galaxy.animate({ transform: radius + "," + position }, time);
-	}
-
+	// a bit of color
+	animate_entpy_world();
 });
 
-function initSnapSvg() {
-	Snap.load("static/website/img/entpy-universe2.svg", onSVGLoaded);
+function validate_document_img() {
+	// function to use .svg or .png image (depending on browser support)
 
-	function onSVGLoaded (svg){
-
-		// http://stackoverflow.com/questions/22091976/creating-a-snap-svg-object-from-an-object-element
-		var objectSvg = Snap("#svg_object");
-		// grab the referenced content
-		// var objectSvgContent = objectSvg.node.contentDocument;
-		// snapify it
-		// console.log(objectSvg.node);
-		var snapifiedSvg = Snap(objectSvg);
-
-		var entpy_logo = snapifiedSvg.select("#g4538-6");
-		entpy_logo.transform('matrix(0.00,0,0,0.00,60,868.50045)');
-		entpy_logo.animate({ transform: 'matrix(0.10,0,0,0.10,35,868.50045)' }, 5000, mina.bounce);
+	if (!Modernizr.svgasimg) {
+		$(".check_image").each(function(index) {
+			var old_src_path;
+			var new_src_path;
+			old_src_path = $(this).attr("src");
+			new_src_path = old_src_path.replace(".svg", ".png");
+			$(this).attr("src", new_src_path);
+		});
 	}
+
+	return true;
+}
+
+function animate_entpy_world() {
+	// entpy logo bounceInDown
+	$(".entpy_logo_image").addClass('animated bounceInDown');
+	$(".entpy_logo_image").css('animation-duration', "3s");
+	$(".entpy_logo_image").css('animation-delay', "1s");
+	$(".entpy_logo_image").css('animation-iteration-count', "1");
+
+	$(".entpy_logo_image").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(param){
+		// entpy logo tada
+		$(this).removeClass("bounceInDown");
+		$(this).addClass('tada');
+		$(this).css('animation-duration', "2s");
+		$(this).css('animation-delay', "1s");
+		$(this).css('animation-iteration-count', "1");
+
+		$(this).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(param){
+			// entpy logo out
+			$(this).removeClass("tada");
+			$(this).addClass('lightSpeedOut');
+			$(this).css('animation-duration', "1s");
+			$(this).css('animation-delay', "1s");
+			$(this).css('animation-iteration-count', "1");
+
+			// scrolling universe to the city
+			setTimeout(function() { page_scrolling(); }, 3000);
+		});
+	});
+
+	return true;
+}
+
+function page_scrolling() {
+
+	// scrolling universe to the city
+	$.scrollTo('.scroll_to_element1', 5500, {easing:'easeOutBounce', onAfter:function(){ logo_swing_enter(); }});//specify an easing equation
+
+	return true;
+}
+
+function logo_swing_enter() {
+	// entpy logo swing in
+
+	$(".entpy_logo_image_pt2").addClass('lightSpeedIn');
+	$(this).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(param){
+		// entpy logo out
+		$(this).removeClass("lightSpeedIn");
+		$(this).addClass('logo_container_after_animation');
+	});
+
+	return true;
 }
