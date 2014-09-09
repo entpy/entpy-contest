bc788f967091231e96a57fc040384ab9a228210b
developer <developer@entpy.com>
2014-09-09 12:47:38 +0200

    test

--- contest/contest/settings.py
+++ contest/contest/settings.py
@@ -14,6 +14,8 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+GIT_PROJECT_NAME = "entpy-contest"
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
@@ -76,30 +78,6 @@ STATICFILES_DIRS = (
 )
 
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
-
-LOGGING = {
-	'version': 1,
-	'disable_existing_loggers': False,
-	'handlers': {
-		'file': {
-			'level': 'DEBUG',
-			'class': 'logging.FileHandler',
-			'filename': '/tmp/debug.log',
-		},
-		'console' : {
-			'class' : 'logging.StreamHandler',
-			'level' : 'INFO',
-			'stream' : 'ext://sys.stdout',
-		},
-	},
-	'loggers': {
-		'django.request': {
-			'handlers': ['file'],
-			'level': 'DEBUG',
-			'propagate': True,
-		},
-	},
-}
 
 # loading local settings
 try:
