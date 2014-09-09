7bb6069ab253d67b407d55d3906458ab8ce9a49d
developer <developer@entpy.com>
2014-09-09 11:56:57 +0200

    cleaning settings.py

--- contest/contest/settings.py
+++ contest/contest/settings.py
@@ -51,17 +51,6 @@ ROOT_URLCONF = 'contest.urls'
 
 WSGI_APPLICATION = 'contest.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-"""
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-"""
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
